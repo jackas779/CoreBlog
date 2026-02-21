e3afe15b029a58a76a09ed1bdc7d67eb7eead882
Nicolas Ardila <jackas779@gmail.com>
2026-02-21 16:32:26 -0500

    Add SQL insert scripts for blog statuses and users

---
+++ src/bd/sql inserts/status_202602211630.sql
@@ -0,0 +1,7 @@
+INSERT INTO api_blog.status (status_name,created_at,last_modify_by_id,last_date_modify,created_by_id,is_active) VALUES
+	 ('is_active','2025-12-03 03:21:37',3,'2025-12-03 03:21:37',3,1),
+	 ('is_inactive','2025-12-03 03:21:37',3,'2025-12-03 03:21:37',3,1),
+	 ('pending_verification','2025-12-03 03:21:37',3,'2025-12-03 03:21:37',3,1),
+	 ('suspend','2025-12-03 03:21:37',3,'2025-12-03 03:21:37',3,1),
+	 ('blocked','2025-12-03 03:21:37',3,'2025-12-03 03:21:37',3,1),
+	 ('desactivated','2025-12-03 03:21:37',3,'2025-12-03 03:21:37',3,1);
---
+++ src/bd/sql inserts/users_202602211630.sql
@@ -0,0 +1,3 @@
+INSERT INTO api_blog.users (username,email,password_hash,first_name,last_name,age,status,create_at,updated_at) VALUES
+	 ('administrato','admin@dominio.com','$2b$10$jfW5XBFR7dcpdeyF/982MeX94Qudg31eVKjE23hnvMbm8G1oAO81K','admin','istrator',18,3,'2025-12-03 03:14:35','2025-12-04 02:47:38'),
+	 ('sample12345','ejemplo@dominio.com','unhashseguro123','juan','perez',18,1,'2025-11-30 15:44:27','2025-12-04 02:47:38');
